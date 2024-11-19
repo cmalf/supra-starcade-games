48aacc95b831e1da1d300e05d97d70ebb4c23a61
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-11-19 13:03:09 +0700

    Update config.js
    add auth0: "auth0|XXXXXXXXXXXXXXXXXXXXXX",

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 
 const config = {
     auth: "Bearer XXXXXXXXXXXXXX",
+    auth0: "auth0|XXXXXXXXXXXXXXXXXXXXXX",
     UserAgent: "Mozilla/5.0 XXXXXXXXXXXXX",
     CountDown: "60" // For Auto Update in second // default 60 seconds
 }
